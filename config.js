dbfbbcbfc4a162713d6802b6188d8febdcb28cae
Thiendeptrai386 <93469195+Thiendeptrai386@users.noreply.github.com>
2021-11-02 10:08:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     btnIntro: 'Click vào đây để tiếp tục',
     title: 'Vui lòng chọn A hoặc B ',
     desc: 'Em có Yêu Anh khôngg? ',
-    btnYes: 'B;Có,yêu anh nhiều ❤<33',
+    btnYes: 'B;Có,yêu anh nhiều ❤<3',
     btnNo: 'A;Đéo,mơ đi cưng :3',
     question:'Em iu ai nhất <3',
     btnReply: 'Gửi cho anh <3',
